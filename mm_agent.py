c32828354d8b6da989f6ad3e6397b73701fbca73
Tom Evslin <tom@evslin.com>
2024-04-08 19:03:52 -0400

    Update mm_agent.py

--- mm_agent.py
+++ mm_agent.py
@@ -215,8 +215,6 @@ class StateMachine:
         last_state=self.chain.get_state(self.thread).next[0].split(':')[0]
         if result is None:
             values=self.chain.get_state(self.thread).values
-            st.write(values)
-            "xxxx"
             print(values)
             return values[last_state]
         return result
